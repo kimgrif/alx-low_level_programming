69a162facfc7b7b71cf638f7d086dcd8f3c71ea4
Griffin <kimuitagriffin@gmail.com>
2023-06-16 00:05:07 +0300

    🐛:debugging: Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
+
 /**
-*main - entry point
- *
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
- *
- *Return; always 0 (success)
-*/
+ * Return: Always 0.
+ */
+
 
 int main(void)
+
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
