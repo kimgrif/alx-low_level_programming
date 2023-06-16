981b97d9ec084e0a5656f95b70cf9eff6369f470
Griffin <kimuitagriffin@gmail.com>
2023-06-16 22:01:54 +0300

    a program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
  * Return: Always 0 (indicating success)
  */
 
-int main(void) {
+int main(void)
+{
 char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
 putchar(alphabet);
 }
 putchar('\n');
-return 0;
+return (0);
 }
