a1b4f11a3c6f1db2ca27bcea28df65cd3039847d
Griffin <kimuitagriffin@gmail.com>
2023-06-17 00:44:52 +0300

    🚩: Inventing is a combination of brains and materials. The more brains you use,the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,27 +2,23 @@
 
 int main(void)
 {
-    int f_num = 0, s_num;
-
-    while (f_num <= 9)
-    {
-        s_num = f_num + 1;
-        while (s_num <= 9)
-        {
-            putchar(f_num + '0');
-            putchar(s_num + '0');
-
-            if (f_num != 8 || s_num != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-
-            ++s_num;
-        }
-        ++f_num;
-    }
-
-    putchar('\n');
-    return 0;
+int f_num = 0, s_num;
+while (f_num <= 9)
+{
+s_num = f_num + 1;
+while (s_num <= 9)
+{
+putchar(f_num + '0');
+putchar(s_num + '0');
+if (f_num != 8 || s_num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+++s_num;
+}
+++f_num;
+}
+putchar('\n');
+return 0;
 }
