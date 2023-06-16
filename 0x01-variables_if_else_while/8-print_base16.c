1f9ab8dda41dcfaf16323ca1106ca19e668f4757
Griffin <kimuitagriffin@gmail.com>
2023-06-16 23:58:16 +0300

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: prints all single digit numbers of base 16
+ *              starting from 0, followed by a new line
+ * Return: Always 0 (indicating success)
+*/
+
+int main(void)
+{
+int a;
+char alphabet;
+for ( a = 0; a <= 9; a++)
+{
+putchar('0' + a);
+}
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
