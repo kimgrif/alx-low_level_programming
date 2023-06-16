81765b0dd60ea4c462a72175ef15a1444435cd8b
Griffin <kimuitagriffin@gmail.com>
2023-06-16 23:11:03 +0300

    🚸:Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: prints all single digit numbers of base 10
+ *              starting from 0, followed by a new line
+ * Return: Always 0 (indicating success)
+*/
+
+int main(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+putchar('0'+a);
+}
+putchar('\n');
+return(0);
+}
