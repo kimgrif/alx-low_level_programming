f171dce3be6182ed876b351e7a0262dc895e7b7a
Griffin <kimuitagriffin@gmail.com>
2023-06-16 18:48:22 +0300

    🎉:task_1:adjusting to meet betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 /**
-* main - variable n will store a different value every time you will run this program 
-* 
-* Description: generates a random number and checks if the number is greater than : is positive, if the number is 0: is zero
-*              if the number is less than 0: is negative
-*
-*             return value is 0
-*/
+ * main - Entry point of the program
+ *
+ * Description: Generates a random number and checks if the number is greater than 0:
+ *              it is positive. If the number is 0, it is zero. If the number is less
+ *              than 0, it is negative.
+ *
+ * Return: Always 0 (indicating success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-    if (n > 0){
-        printf("%d is positive\n", n);
-    } else if (n < 0) {
-        printf("%d is negative\n", n);
-    } else {
-        printf("%d is zero\n", n);
-    }
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
 	return (0);
 }
