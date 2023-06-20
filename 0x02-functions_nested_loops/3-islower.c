8a914a51e56a41e635e6e8688c9e2a67e4207b61
Griffin <kimuitagriffin@gmail.com>
2023-06-20 15:52:50 +0300

    🎉:adds int _islower(int c)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ *int _islower - Returns 1 if c is lowercase
+ *
+ * Return: Always 0.
+ */
+
+
+int _islower(int c)
+{
+char x;
+if (x == 'c')
+{
+return (1);
+}
+return (0);
+}
