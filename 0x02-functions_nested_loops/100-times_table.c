8f2bea2751cd5bb38b21d45a1e562c8bb1d34824
Griffin <kimuitagriffin@gmail.com>
2023-06-20 23:09:26 +0300

    💚🐛🔨:The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,33 +8,34 @@
 
 void print_times_table(int n)
 {
-int x, y, z;
-if (n <= 15 && n >= 0)
-{
-for (z = 0; z <= n; ++z)
-{
-_putchar(48);
-for (y = 1; y <= n; ++y)
-{
-_putchar(',');
-_putchar(' ');
-x = z * y;
-if (x <= 9)
-_putchar(' ');
-if (x <= 99)
-_putchar(' ');
-if (x >= 100)
-{
-_putchar((x / 100) + 48);
-_putchar((x / 10) % 10 + 48);
-}
-else if (x <= 99 && x >= 10)
-{
-_putchar((x / 10) + 48);
-_putchar((x % 10) + 48);
-}
-_putchar('\n');
-}
-}
-}
+	int x, y, z;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (z = 0; z <= n; ++z)
+		{
+			_putchar(48);
+			for (y = 1; y <= n; ++y)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				x = z * y;
+
+				if (x <= 9)
+					_putchar(' ');
+				if (x <= 99)
+					_putchar(' ');
+
+				if (x >= 100)
+				{
+					_putchar((x / 100) + 48);
+					_putchar((x / 10) % 10 + 48);
+				} else if (x <= 99 && x >= 10)
+					_putchar((x / 10) + 48);
+				_putchar((x % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
 }
