def36bf50d78471655dce7c3b3642115c58534b1
Griffin <kimuitagriffin@gmail.com>
2023-06-20 10:48:36 +0300

    ⚡️:0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:  prints _putchar, followed by a new line
+ *              from a ... z
+ *
+ * Return: Always 0 (indicating success)
+ */
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
