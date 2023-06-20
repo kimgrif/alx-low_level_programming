27bcf364b922df328d68a9af391dacd2337011ae
Griffin <kimuitagriffin@gmail.com>
2023-06-20 17:05:00 +0300

    🚨:💚:Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: The number to be checked
+ *
+ * Return: 1 if the number is +, 0 if it is 0, -1 if it is -
+ */
+int print_sign(int n)
+{
+int a = (int)n;
+if (a > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (a == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (0);
+}
+}
