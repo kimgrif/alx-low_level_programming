bf07016864bd355ba6c3e24d1feda1b9357ca2c0
Griffin <kimuitagriffin@gmail.com>
2023-06-24 00:35:35 +0300

    🍻:Numbers constitute the only universal languag.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,14 @@
 */
 void more_numbers(void)
 {
-int i;
-int j = 0;
-while (j < 10)
+int i, j;
+for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 14; i++)
 {
 if (i < 10)
 _putchar((i / 10) + '0');
 _putchar((i % 10) + '0');
-j++;
 }
 _putchar('\n');
 }
