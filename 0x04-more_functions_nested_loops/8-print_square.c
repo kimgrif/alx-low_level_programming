fc0595b12deb2df463eda513979605a884ffdb4c
Griffin <kimuitagriffin@gmail.com>
2023-06-24 01:07:45 +0300

    🏗️ :You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_square- prints a square line in the terminal.
+ *@n: number being checked
+ *Return:  (Success)
+*/
+void print_square(int size)
+{
+if (size >= 1)
+{
+int a, b;
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
