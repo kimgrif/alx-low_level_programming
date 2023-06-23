1384cf1a63bbc7fcd145aa755e19496df2175a34
Griffin <kimuitagriffin@gmail.com>
2023-06-23 22:08:52 +0300

    🎉:isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isdigit - Returns 1 if c is digit else returns 0
+ *@c: The character to be checked
+ *Return:  (Success)
+*/
+
+int _isdigit(int c)
+{
+for (c = 0; c >= 9; c++)
+{
+return (1);
+}
+return (0);
+}
