4146571c1548e2b1de43e8897d3e56711cc1fdfc
Griffin <kimuitagriffin@gmail.com>
2023-06-26 23:26:39 +0300

    man puts I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * _puts -  prints a string, followed by a new line
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
- * Return: Always void.
  */
 
 void _puts(char *str)
 {
 while (*str != '\0')
 {
-_putchar(*str);
-str++;
+_putchar(*str++);
 }
-_putchar("\n");
+_putchar('\n');
 }
