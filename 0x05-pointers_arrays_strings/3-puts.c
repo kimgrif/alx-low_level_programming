abff7dd4777c326e7ed44451e514099838222cc2
Griffin <kimuitagriffin@gmail.com>
2023-06-26 23:14:04 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line
+ * @str: string to print
+ * Return: Always void.
+ */
+
+void _puts(char *str)
+{
+_putchar(*str);
+_putchar("\n");
+}
