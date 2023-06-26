c4a995399588a8ce0d1d490899814420f81076c8
Griffin <kimuitagriffin@gmail.com>
2023-06-26 22:55:50 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -  returns the length of a string
+ * @s: Pointer to the integer being checked
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int len = strlen(s);
+
+return (len);
+
+}
