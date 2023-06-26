d2c1b372fffe6e7f613e255c4ee545918900e685
Griffin <kimuitagriffin@gmail.com>
2023-06-27 00:16:14 +0300

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - prints every other character of a string
+ *              starting with the first character,
+ *              followed by a new line
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+int a, b, counter;
+counter = 0;
+for (a = 0; str[a] != '\0'; a++)
+counter++;
+b = (counter / 2);
+if ((counter % 2) == 1)
+b = ((counter + 1) / 2);
+for (a = b; str[a] != '\0'; a++)
+_putchar(str[a]);
+_putchar('\n');
+}
