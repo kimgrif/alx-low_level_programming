6c550678eca75e49ba6579c57f50215bf395c51f
Griffin <kimuitagriffin@gmail.com>
2023-06-30 22:46:14 +0300

    0. strcat

---
+++ 0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	a = 0;
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
