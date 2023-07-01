9b5ff71c0a36b3ea3087508f950a4397e0eca1aa
Griffin <kimuitagriffin@gmail.com>
2023-07-01 13:20:58 +0300

    🚀:Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes
+ *              all words of a string
+ *
+ * @s: pointer to char input array
+ *
+ * Return: @s
+*/
+char *cap_string(char *n)
+{
+	int index = 0;
+
+	while (n[index])
+	{
+		while (!(n[index] >= 'a' && n[index] <= 'z'))
+			index++;
+
+		if (n[index - 1] == ' ' ||
+		    n[index - 1] == '\t' ||
+		    n[index - 1] == '\n' ||
+		    n[index - 1] == ',' ||
+		    n[index - 1] == ';' ||
+		    n[index - 1] == '.' ||
+		    n[index - 1] == '!' ||
+		    n[index - 1] == '?' ||
+		    n[index - 1] == '"' ||
+		    n[index - 1] == '(' ||
+		    n[index - 1] == ')' ||
+		    n[index - 1] == '{' ||
+		    n[index - 1] == '}' ||
+		    index == 0)
+			n[index] -= 32;
+
+		index++;
+	}
+
+	return (n);
+}
