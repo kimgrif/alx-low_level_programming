8812eb714e700ce3213f05cee25b12d56e29a5fa
Griffin <kimuitagriffin@gmail.com>
2023-07-04 22:37:27 +0300

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n byted changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int a;
+for (a = 0; a < n; a++)
+dest[a] = src[a];
+return (dest);
+}
