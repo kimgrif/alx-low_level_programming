13abd339eb9a53623b2186af6c8033752a7442a4
Griffin <kimuitagriffin@gmail.com>
2023-07-05 22:49:14 +0300

    💡: Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,31 @@
 #include "main.h"
-int func_sqrt_recursion(int n, int a);
-/**
- * _sqrt_recursion- returns the natural square root of a number
- * @n: input value
- * Return: returns the natural square root of n
-*/
 
+int func_sqrt_recursion(int n, int a);
+
+/**
+ * _sqrt_recursion - Returns the square root of a number
+ * @n: The number to calculate the square root of
+ * Return: The square root of n (or -1 if non-perfect square or invalid input)
+ */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-return (func_sqrt_recursion(0, n));
+    if (n < 0)
+        return -1;
+    return func_sqrt_recursion(n, 0);
 }
+
 /**
- * func_sqrt_recursion- recurses to find the natural
- *                      square root of a number
- * @a: iterator
- * @n: input value
- * Return: the resulting square root
-*/
+ * func_sqrt_recursion - Recursively searches for the square root of a number
+ * @n: The number to calculate the square root of
+ * @a: The current value being tested as the square root
+ * Return: The square root of n (or -1 if non-perfect square)
+ */
 int func_sqrt_recursion(int n, int a)
 {
-if (a * a > n)
-return (-1);
-if (a * a == n)
-return (a);
-return (func_sqrt_recursion(n, a + 1));
+    if (a * a == n)
+        return a;
+    if (a * a > n)
+        return -1;
+    return func_sqrt_recursion(n, a + 1);
 }
+
