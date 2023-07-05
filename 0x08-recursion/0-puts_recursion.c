a7fcde3ab9fecb0ca1fdc84a21530e4fa4be236d
Griffin <kimuitagriffin@gmail.com>
2023-07-05 13:15:11 +0300

    🎉:She locked away a secret, deep inside herself something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_puts_recursion - prints a string, followed by a new line.
+*
+* @s: input value
+*return - return s
+*/
+void _puts_recursion(char *s)
+{
+_putchar(s);
+_putchar('/n');
+}
