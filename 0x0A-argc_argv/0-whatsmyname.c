d78b5be3a470b7e5594fba3053d2049f795cf317
Griffin <kimuitagriffin@gmail.com>
2023-07-07 23:09:14 +0300

    🎉:It ain't what they call you, it's what you answer to..

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,9 @@
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", argv[0]);
-    return 0;
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
