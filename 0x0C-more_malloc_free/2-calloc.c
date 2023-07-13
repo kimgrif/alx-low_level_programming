c9e3f8140a44b4247b7513cba75de5c2361f32ab
Griffin <kimuitagriffin@gmail.com>
2023-07-13 22:01:15 +0300

    ✅ :🔖: array_range

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * *_memset - fills memory with a constant byte
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
+ *
+ * Return: pointer to the new array
+ */
+
+int *array_range(int min, int max)
+{
+	int i, *array;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc((max - min + 1) * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		array[i] = min;
+		min++;
+	}
+	return (array);
+}
