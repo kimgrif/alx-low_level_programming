0eab6d0e049b90690280ad018063f20374c4a7ec
Griffin <kimuitagriffin@gmail.com>
2023-07-13 22:04:08 +0300

    🔥✅ :🔖: array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,19 +10,16 @@
 
 int *array_range(int min, int max)
 {
-	int i, *array;
-
-	if (min > max)
-		return (NULL);
-
-	array = malloc((max - min + 1) * sizeof(int));
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; min <= max; i++)
-	{
-		array[i] = min;
-		min++;
-	}
-	return (array);
+int i, *array;
+if (min > max)
+return (NULL);
+array = malloc((max - min + 1) * sizeof(int));
+if (array == NULL)
+return (NULL);
+for (i = 0; min <= max; i++)
+{
+array[i] = min;
+min++;
+}
+return (array);
 }
