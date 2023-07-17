e605db03f780adbce19e07fa1d0186cec4ef96ee
Griffin <kimuitagriffin@gmail.com>
2023-07-17 22:15:14 +0300

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - a new type struct dog with the following elements
+ */
+typedef struct dog dog_t;
+
+#endif
